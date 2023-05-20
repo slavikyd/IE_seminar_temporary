aa4f49cb7ea78c570c14237264a74ff7dc7467b2
MaybeCaat <vladislav.osipov.023@yandex.ru>
2023-05-20 09:38:57 +0300

    add script getting arguments

--- script.sh
+++ script.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+while [[ -n $1 ]]
+do
+	case $1 in
+		-f) input=$2
+        shift;;
+		-o) output=$2
+        shift;;
+		-t) seats=$2
+        shift;;
+		*) echo "Unknown argument $1"
+        shift;;
+	esac
+	shift
+done
+
+if [[ ! -f $input ]]
+then
+    echo "$input is not file"
+    exit 1
+fi
+
+if [[ ! -f $output ]]
+then
+    touch $output
+fi
